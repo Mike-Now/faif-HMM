e1cfa2ca885d4114d2ffe3617ea158bcf00acc64
Attil <attil2@gmail.com>
2014-12-07 15:23:52 +0000

    Some more skeleton of MaxEnt Classifier

--- dev/MaxEnt.hpp
+++ dev/MaxEnt.hpp
@@ -1,3 +1,6 @@
+#ifndef FAIF_MAXENT_HPP
+#define FAIF_MAXENT_HPP
+
 #include <Classifier.hpp>
 
 namespace faif {
@@ -6,29 +9,37 @@ namespace faif {
 		template<typename Val>
 		class MaxEnt : public Classifier<Val> {
 		public:
-            typedef typename Classifier<Val>::AttrValue AttrValue;
-            typedef typename Classifier<Val>::AttrDomain AttrDomain;
-            typedef typename Classifier<Val>::AttrIdd AttrIdd;
-            typedef typename Classifier<Val>::AttrIddSerialize AttrIddSerialize;
-            typedef typename Classifier<Val>::Domains Domains;
-            typedef typename Classifier<Val>::Beliefs Beliefs;
-            typedef typename Classifier<Val>::ExampleTest ExampleTest;
-            typedef typename Classifier<Val>::ExampleTrain ExampleTrain;
-            typedef typename Classifier<Val>::ExamplesTrain ExamplesTrain;
+			typedef typename Classifier<Val>::AttrValue AttrValue;
+			typedef typename Classifier<Val>::AttrDomain AttrDomain;
+			typedef typename Classifier<Val>::AttrIdd AttrIdd;
+			typedef typename Classifier<Val>::AttrIddSerialize AttrIddSerialize;
+			typedef typename Classifier<Val>::Domains Domains;
+			typedef typename Classifier<Val>::Beliefs Beliefs;
+			typedef typename Classifier<Val>::ExampleTest ExampleTest;
+			typedef typename Classifier<Val>::ExampleTrain ExampleTrain;
+			typedef typename Classifier<Val>::ExamplesTrain ExamplesTrain;
 
 		public:
 			MaxEnt();
-			MaxEnt(const Domains& attr_domains, const AttrDomain& category_domains);
+			MaxEnt(const Domains& attr_domains, const AttrDomain& category_domains, unique_ptr<MaxEntTraining> trainer);
+			virtual ~MaxEnt() { }
 
+			virtual void reset();
 
-		class MaxEntTraining {
-		public:
+		private:
+			unique_ptr<MaxEntTraining> impl_;
 
-		};
+			//interface
+			class MaxEntTraining {
+			public:
 
-		class MaxEntClassify : public MaxEntTraining {
+			};
 
-		};
+			class MaxEntClassify : public MaxEntTraining {
+
+			};
 		};
 	}
 }
+
+#endif
